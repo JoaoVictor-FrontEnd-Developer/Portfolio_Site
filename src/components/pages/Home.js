654eae55ce7241fa73acdac953accdebd63c6ee2
João Victor <joaovictor.developer.frontend@gmail.com>
2023-06-21 15:21:43 -0300

    curriculo atualizado

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -25,7 +25,7 @@ function Home() {
                         <div className={styles.buttons}>
                             <a href='https://www.linkedin.com/in/jo%C3%A3o-victor-632a1021a/' target='_blank' rel="noreferrer"><FaLinkedin/> Linkedin</a>
                             <a href='https://github.com/JoaoVictor-FrontEnd-Developer' target='_blank' rel="noreferrer"><FaGithub/> Github</a>
-                            <a href='https://drive.google.com/file/d/1caHzCk96ga-ixhbKxSX9lY1EkCGHGb4X/view?usp=sharing' target='_blank' rel="noreferrer"><FaDownload/> Currículo</a>
+                            <a href='https://drive.google.com/file/d/1R5zxReYHtgw5DlHOKKDqwlWEvQwrhA5w/view?usp=sharing' target='_blank' rel="noreferrer"><FaDownload/> Currículo</a>
                         </div>
                     </div>
 
